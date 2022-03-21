24af2ae7fc00bbad65928e427fefd2e5acb35041
kotahorii <khorii.micron@gmail.com>
2022-03-21 19:36:01 +0900

    [create] rand.go

--- db/query/account.sql
+++ db/query/account.sql
@@ -17,3 +17,12 @@ ORDER BY id
 LIMIT $1
 OFFSET $2;
 
+-- name: UpdateAccount :exec
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
